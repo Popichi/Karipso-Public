f9e2eac09634134071913d365463f84ca3401b9e
Karipso <amaral@ucsc.edu>
2019-08-04 18:22:31 -0700

    Finished the implementation of a simple doubly-linked unsorted linked list in c++ and ran a very simple test tharness for it.

--- cpp_practice_codes/data_structures/doubly_linked_unsorted_ll/anil_doubly_linked_unsorted_ll.h
+++ cpp_practice_codes/data_structures/doubly_linked_unsorted_ll/anil_doubly_linked_unsorted_ll.h
@@ -1,6 +1,8 @@
 /* This is an implementation of linked list as a dynamically growing
    unsorted and doubly-linked list! */
 
+/* TO DO: 1) Should I do anything with the tail member?*/
+
 #ifndef ANIL_DOUBLY_LINKED_UNSORTED_LL_H
 #define ANIL_DOUBLY_LINKED_UNSORTED_LL_H
 
@@ -8,14 +10,14 @@
 
 namespace anil {
 
+    struct node {
+        int data;
+        node* next;
+        node* prev;
+    };
+
     class linked_list {
         private:
-
-            struct node {
-                int key;
-                node* next;
-                node* prev;
-            };
 
             node* head;
             node* tail;
@@ -26,8 +28,8 @@ namespace anil {
         public:
             linked_list() : head(NULL), tail(NULL) {}
             void insert(int new_data);
-            void remove();
-            node* search();
+            void remove(node* x);
+            node* search(int data);
             ~linked_list();
     };
 }
